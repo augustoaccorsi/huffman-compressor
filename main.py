d11b6c446c42f874e3af6abd1f6585e294b339e9
augustoaccorsi <augusto.acorsi@gmail.com>
2019-09-03 09:18:36 -0300

    add zip funcionality using zipfile library

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ def zipdir(path, ziph):
             ziph.write(os.path.join(root, file))
 
 if __name__ == '__main__':
-    zipf = zipfile.ZipFile('files.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipdir('files', zipf)
+    zipf = zipfile.ZipFile('sum.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('files/sum/', zipf)
     zipf.close()
+
+    zipf = zipfile.ZipFile('alice.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('files/alice/', zipf)
+    zipf.close()
+
+""" import os
+import zipfile
+
+def zipdir(path, outputfile):
+    # ziph is zipfile handle
+    zipf = zipfile.ZipFile(outputfile, 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zipf.write(os.path.join(root, file))
+    zipf.close()
+
+
+zipdir('file/sum', 'sum.zip')
+zipdir('files/sum/sum', 'alice.zip') """
