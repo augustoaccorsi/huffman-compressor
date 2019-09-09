523dfb2a22acabddeab0d2eb5fdc49fc32cc1214
augustoaccorsi <augusto.acorsi@gmail.com>
2019-09-09 19:05:18 -0300

    add crc function

--- Huffman.py
+++ Huffman.py
@@ -4,12 +4,22 @@ import os
 from HeapNode import HeapNode
 from HuffmanDecompressor import HuffmanDecompressor
 from HuffmanCompressor import HuffmanCompressor
+import zlib
 
-#input file path
+def calculate_crc(fileName):
+    prev = 0
+    for eachLine in open(fileName,"rb"):
+        prev = zlib.crc32(eachLine, prev)
+    return "%X"%(prev & 0xFFFFFFFF)
+
 path = sys.argv[1]
 
 hc = HuffmanCompressor(path)
 output_path = hc.compress()
 
+print(calculate_crc(output_path))
+
 hd = HuffmanDecompressor(hc)
 hd.decompress(output_path)
+
+
